d3550f350b496acc245a5805d61a805b48f44c5a
Gayan65 <gayan65728@yahoo.com>
2024-07-10 22:15:52 +0300

    db connection established

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT;
+const db_url = process.env.DB_URL;
 
-//server
-app.listen(port, () => {
-    console.log(`server is running on port no: ${port}`);
-});
+//db connection
+mongoose
+    .connect(db_url)
+    .then(() => {
+        //server
+        app.listen(port, () => {
+            console.log(`DB connected, server is running on port no: ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
