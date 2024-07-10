1b4c9ede48b4a56663c42015a745891cd1d6e4b5
Gayan65 <gayan65728@yahoo.com>
2024-07-10 23:21:13 +0300

    test API add

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,22 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+import userRouter from "./routes/user.js";
+
+//express app
 const app = express();
+
+//envs
 const port = process.env.PORT;
 const db_url = process.env.DB_URL;
+
+//middleware
+app.use((req, res, next) => {
+    console.log(`Request method : ${req.method}, Request path: ${req.path}`);
+    next();
+});
+
+app.use("/api/user", userRouter);
 
 //db connection
 mongoose
