13cb4ac6941d235dccd6ed16173bd2d21eb56c02
Gayan65 <gayan65728@yahoo.com>
2024-07-11 22:02:23 +0300

    user login signin static functions add

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const { Schema } = mongoose;
 
@@ -6,5 +7,37 @@ const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
+
+//static methods
+
+userSchema.statics.signup = async function (email, password) {
+    // check bank fields
+    if (!email || !password) {
+        throw Error("Field can not be blank!");
+    }
+
+    // check email
+    if (!validator.isEmail(email)) {
+        throw Error("email is not valid!");
+    }
+
+    // check password
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is not strong!");
+    }
+
+    //check the existence of the email
+    const exist = await this.findOne({ email });
+    if (exist) {
+        throw Error("Email already exists!");
+    }
+
+    //send to db for creating user
+    const user = await this.create({ email, password });
+
+    return user;
+};
+
+userSchema.statics.login = async function (email, password) {};
 
 export const User = mongoose.model("User", userSchema);
