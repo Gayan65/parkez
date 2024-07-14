d569bcad11768af7379ec0b6d5a35f1371f37343
Gayan65 <gayan65728@yahoo.com>
2024-07-14 12:10:09 +0300

    login - css, basic form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Login = () => {
-    return <div>Login page</div>;
+    return (
+        <div className="login-container">
+            <div className="login-form">
+                <h2 className="text-center">Login</h2>
+                <form>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Enter email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            autoComplete="true"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-primary btn-block mt-3"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
