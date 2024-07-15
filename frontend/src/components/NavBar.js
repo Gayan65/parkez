452078e1293f45a006b8e7d493c0bab8f5b62885
Gayan65 <gayan65728@yahoo.com>
2024-07-15 13:24:03 +0300

    navbar logout, sigup, login add, user has hold in the context while refreshing fixed

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,41 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const NavBar = () => {
+    //user logout
+    const { logout } = useLogout();
+
+    const handelClick = () => {
+        logout();
+    };
+
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to={"/"}>
                     Navbar
                 </Link>
+                <div className="flex-d">
+                    <Link
+                        className="btn btn-outline-primary me-1"
+                        to={"/login"}
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        className="btn btn-outline-primary me-1"
+                        to={"/signup"}
+                    >
+                        Sign up
+                    </Link>
+                    <span>User : </span>
+                    <button
+                        className="btn btn-outline-primary"
+                        onClick={handelClick}
+                    >
+                        Logout
+                    </button>
+                </div>
             </div>
         </nav>
     );
