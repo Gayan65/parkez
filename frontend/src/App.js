3d6bdc76de9b3890e862b7c1f8e203dde5636527
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-07-31 20:33:56 +0300

    frontend: create page for admin, user admin trigers in Nav bar - backend: returns admin status with the user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,22 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import CreatePage from "./pages/admin/CreatePage";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,55 +3,69 @@ import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 const NavBar = () => {
-    //get user
-    const { user } = useAuthContext();
+  //get user
+  const { user } = useAuthContext();
 
-    //user logout
-    const { logout } = useLogout();
+  //user logout
+  const { logout } = useLogout();
 
-    const handelClick = () => {
-        logout();
-    };
+  const handelClick = () => {
+    logout();
+  };
 
-    return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to={"/"}>
-                    Navbar
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={"/"}>
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              {user && user.admin && (
+                <Link className="nav-link active" to={"/create"}>
+                  Parking Management
                 </Link>
-                <div className="flex-d">
-                    {!user && (
-                        <div>
-                            {" "}
-                            <Link
-                                className="btn btn-outline-primary me-1"
-                                to={"/login"}
-                            >
-                                Login
-                            </Link>
-                            <Link
-                                className="btn btn-outline-primary me-1"
-                                to={"/signup"}
-                            >
-                                Sign up
-                            </Link>
-                        </div>
-                    )}
-                    {user && (
-                        <div>
-                            <span>User : {user.email}</span>{" "}
-                            <button
-                                className="btn btn-outline-primary"
-                                onClick={handelClick}
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    )}
-                </div>
+              )}
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex-d">
+          {!user && (
+            <div>
+              {" "}
+              <Link className="btn btn-outline-primary me-1" to={"/login"}>
+                Login
+              </Link>
+              <Link className="btn btn-outline-primary me-1" to={"/signup"}>
+                Sign up
+              </Link>
             </div>
-        </nav>
-    );
+          )}
+          {user && (
+            <div>
+              <span>User : {user.email}</span>{" "}
+              <button className="btn btn-outline-primary" onClick={handelClick}>
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
