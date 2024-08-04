d62e80d5bde48fe37cb16ebce1526923b2db76b7
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-08-04 11:11:26 +0300

    fetch buildingd add to the buidling create page

--- frontend/src/pages/admin/CreateBuildingPage.js
+++ frontend/src/pages/admin/CreateBuildingPage.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CreateBuildingForm from "../../components/CreateBuildingForm";
 
 const CreateBuildingPage = () => {
-  //Display all the Buildings here.. and create building for component to be imported here..
+  //set state
+  const [buildings, setBuildings] = useState([]);
+
+  useEffect(() => {
+    //api call to get all buildings
+    const fetchBuildings = async () => {
+      const response = await fetch("/api/building/");
+      const json = await response.json();
+
+      if (response.ok) {
+        setBuildings(json);
+      }
+    };
+
+    fetchBuildings();
+  }, []);
+
   return (
     <div>
       CreateBuildingPage
       <div>
         <CreateBuildingForm />
+      </div>
+      <div>
+        {buildings && buildings.map((building) => <div> {building.name} </div>)}
       </div>
     </div>
   );
