9b231f3a6250d32219704009f41e916bcc30cabc
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-08-17 09:42:04 +0300

    radio parking lot id trigger

--- frontend/src/components/ParkingLotDetailsRadio.js
+++ frontend/src/components/ParkingLotDetailsRadio.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const ParkingLotDetailsRadio = ({ lot, status, i }) => {
-    return (
-        <div>
-            <input
-                type="radio"
-                className="btn-check"
-                name="btnradio"
-                id={i}
-                autoComplete="off"
-            />
-            <label className="btn btn-outline-primary" htmlFor={i}>
-                {lot}, {status}
-            </label>
-        </div>
-    );
+const ParkingLotDetailsRadio = ({ lot, status, i, onSelect }) => {
+  return (
+    <div>
+      <input
+        type="radio"
+        className="btn-check"
+        name="btnradio"
+        id={i}
+        autoComplete="off"
+        onChange={onSelect}
+      />
+      <label className="btn btn-outline-primary" htmlFor={i}>
+        {lot}, {status}
+      </label>
+    </div>
+  );
 };
 
 export default ParkingLotDetailsRadio;
--- frontend/src/pages/ParkingSelect.js
+++ frontend/src/pages/ParkingSelect.js
@@ -1,51 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { useParkLotContext } from "../hooks/useParkLotContext";
 import ParkingLotDetailsRadio from "../components/ParkingLotDetailsRadio";
 
 const ParkingSelect = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    const { parks, park_dispatch } = useParkLotContext();
-    //accessing the state via location
-    const { building, apartment, room } = location.state;
+  const { parks, park_dispatch } = useParkLotContext();
+  //accessing the state via location
+  const { building, apartment, room } = location.state;
 
-    useEffect(() => {
-        //fetch all parking lots
-        const fetchAllParking = async () => {
-            const response = await fetch(`/api/park/${building}`);
-            const json = await response.json();
+  //states for select parking lots
+  const [selectParkingLot, setSelectParkingLot] = useState("");
 
-            if (response.ok) {
-                park_dispatch({ type: "SET_PARKS", payload: json });
-            }
-        };
+  useEffect(() => {
+    //fetch all parking lots
+    const fetchAllParking = async () => {
+      const response = await fetch(`/api/park/${building}`);
+      const json = await response.json();
 
-        fetchAllParking();
-    }, [park_dispatch, building]);
-    return (
-        <div>
-            Parking select Building{building} , Apartment{apartment}, Room{room}
-            <div className="container text-center">
-                <div
-                    className="row row-cols-3 btn-group"
-                    role="group"
-                    aria-label="Basic radio toggle button group"
-                >
-                    {parks &&
-                        parks.map((parkingLot, i) => (
-                            <ParkingLotDetailsRadio
-                                key={parkingLot._id}
-                                lot={parkingLot.lot}
-                                status={parkingLot.status}
-                                i={i}
-                            />
-                        ))}
-                </div>
-            </div>
+      if (response.ok) {
+        park_dispatch({ type: "SET_PARKS", payload: json });
+      }
+    };
+
+    fetchAllParking();
+  }, [park_dispatch, building]);
+
+  const handleClick = () => {
+    //selected parking lot details triggered
+    console.log("request btn clicked");
+    console.log(selectParkingLot);
+  };
+  return (
+    <div>
+      Parking select Building{building} , Apartment{apartment}, Room{room}
+      <div className="container text-center">
+        <div
+          className="row row-cols-3 btn-group"
+          role="group"
+          aria-label="Basic radio toggle button group"
+        >
+          {parks &&
+            parks.map((parkingLot, i) => (
+              <ParkingLotDetailsRadio
+                key={parkingLot._id}
+                lot={parkingLot.lot}
+                status={parkingLot.status}
+                i={i}
+                onSelect={() => setSelectParkingLot(parkingLot._id)}
+              />
+            ))}
         </div>
-    );
+      </div>
+      <button className="btn btn-primary" onClick={handleClick}>
+        Request
+      </button>
+    </div>
+  );
 };
 
 export default ParkingSelect;
