5bad8810ba89d260beffb6bd8ab2415bbb1c4847
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-08-23 21:00:10 +0300

    parkinglot model updated, user parameter add

--- backend/models/ParkLotModel.js
+++ backend/models/ParkLotModel.js
@@ -3,22 +3,24 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const parkLotSchema = new Schema(
-    {
-        lot: {
-            type: Number,
-            required: true,
-            unique: true,
-        },
-        status: {
-            type: String,
-            required: true,
-        },
-        building_id: {
-            type: String,
-            required: true,
-        },
+  {
+    lot: {
+      type: Number,
+      required: true,
     },
-    { timestamps: true }
+    status: {
+      type: String,
+      required: true,
+    },
+    building_id: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 export const ParkLot = mongoose.model("ParkLot", parkLotSchema);
