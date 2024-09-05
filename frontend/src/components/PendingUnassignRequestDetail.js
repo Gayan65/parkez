f05170f5e4055954f287fbe90c0d1e83bedef643
Gayan65 <gayan65728@yahoo.com>
2024-09-05 18:03:09 +0300

    trigger the approve, decline buttons

--- frontend/src/components/PendingUnassignRequestDetail.js
+++ frontend/src/components/PendingUnassignRequestDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PendingUnassignRequestDetail = ({
     building,
@@ -11,6 +11,23 @@ const PendingUnassignRequestDetail = ({
     parkingLot_id,
     status,
 }) => {
+    const [comment, setComment] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const action = e.nativeEvent.submitter.value;
+
+        //handle approve button
+        if (action === "approve") {
+            console.log("approve", comment);
+        }
+
+        //handle decline button
+        if (action === "decline") {
+            console.log("decline", comment);
+        }
+    };
     return (
         <div className="card">
             <h5 className="card-header">{building}</h5>
@@ -23,8 +40,19 @@ const PendingUnassignRequestDetail = ({
                 <p className="card-text">Parking Lot Number: {parkingLot}</p>
                 <p className="card-text">Parking lot id : {parkingLot_id}</p>
                 <p className="card-text">Status : {status}</p>
-                <button className="btn btn-primary">Approve</button>
-                <button className="btn btn-danger">Decline</button>
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        className="form-control"
+                        rows="2"
+                        onChange={(e) => setComment(e.target.value)}
+                    ></textarea>
+                    <button className="btn btn-primary" value={"approve"}>
+                        Approve
+                    </button>
+                    <button className="btn btn-danger" value={"decline"}>
+                        Decline
+                    </button>
+                </form>
             </div>
         </div>
     );
