0cf396e6f647c3953be2b598b0fcd47f81f11b27
Gayan65 <gayan65728@yahoo.com>
2024-09-25 14:48:02 +0300

    tasks tabs add

--- frontend/src/pages/admin/Tasks.js
+++ frontend/src/pages/admin/Tasks.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+//components
+import PendingRequest from "../../components/PendingRequest";
+import PendingUnassignRequest from "../../components/PendingUnassignRequest";
 
 const Tasks = () => {
-    return <div>Tasks page</div>;
+    const { user } = useAuthContext();
+    const [activeTab, setActiveTab] = useState("assign");
+
+    return (
+        <div className="container">
+            {/* Nav tabs */}
+            <ul className="nav nav-tabs" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button
+                        className={`nav-link ${
+                            activeTab === "assign" ? "active" : ""
+                        }`}
+                        onClick={() => setActiveTab("assign")}
+                        role="tab"
+                        type="button"
+                    >
+                        Parking Assign Requests
+                    </button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button
+                        className={`nav-link ${
+                            activeTab === "unassign" ? "active" : ""
+                        }`}
+                        onClick={() => setActiveTab("unassign")}
+                        role="tab"
+                        type="button"
+                    >
+                        Parking Unassign Requests
+                    </button>
+                </li>
+            </ul>
+
+            {/* Conditionally render the appropriate component */}
+            <div className="tab-content mt-3">
+                {activeTab === "assign" && user && user.admin && (
+                    <PendingRequest />
+                )}
+                {activeTab === "unassign" && user && user.admin && (
+                    <PendingUnassignRequest />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Tasks;
