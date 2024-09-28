0b36a8e3a667e05c6d8d6ad6d4a8b4544b6ceb86
Gayan65 <gayan65728@yahoo.com>
2024-09-28 19:41:38 +0300

    Loader spinner add

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,12 @@
 import React from "react";
 
 //components
-import { useAuthContext } from "../hooks/useAuthContext";
 import LandingPara from "../components/LandingPara";
-import PendingRequest from "../components/PendingRequest";
-import PendingUnassignRequest from "../components/PendingUnassignRequest";
 
 const Home = () => {
-    const { user } = useAuthContext();
     return (
         <div>
             <LandingPara />
-
-            {user && user.admin && <PendingRequest />}
-
-            {user && user.admin && <PendingUnassignRequest />}
         </div>
     );
 };
