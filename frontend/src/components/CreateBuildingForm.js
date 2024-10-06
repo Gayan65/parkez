2162209a8016c48a3c2c50eb11cfbc2b4242fb11
Gayan65 <gayan65728@yahoo.com>
2024-10-06 21:18:15 +0300

    css add to the parking mgt form

--- frontend/src/components/CreateBuildingForm.js
+++ frontend/src/components/CreateBuildingForm.js
@@ -2,117 +2,119 @@ import React, { useState } from "react";
 import { useBuildingsContext } from "../hooks/useBuildingsContext";
 
 const CreateBuildingForm = () => {
-  const { dispatch } = useBuildingsContext();
-  //set states
+    const { dispatch } = useBuildingsContext();
+    //set states
 
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [address, setAddress] = useState("");
-  const [image, setImage] = useState("");
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [address, setAddress] = useState("");
+    const [image, setImage] = useState("");
 
-  const [error, setError] = useState(null);
-  const [emptyField, setEmptyField] = useState([]);
+    const [error, setError] = useState(null);
+    const [emptyField, setEmptyField] = useState([]);
 
-  //form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    //form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const building = { name, number, image, address };
+        const building = { name, number, image, address };
 
-    //API call to backend
-    const response = await fetch("api/building/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(building),
-    });
+        //API call to backend
+        const response = await fetch("api/building/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(building),
+        });
 
-    const json = await response.json();
+        const json = await response.json();
 
-    if (!response.ok) {
-      setError(json.error);
-      console.log(json);
-      setEmptyField(json.emptyFields);
-    }
+        if (!response.ok) {
+            setError(json.error);
+            console.log(json);
+            setEmptyField(json.emptyFields);
+        }
 
-    if (response.ok) {
-      setName("");
-      setNumber("");
-      setImage("");
-      setAddress("");
-      setError(null);
-      setEmptyField([]);
+        if (response.ok) {
+            setName("");
+            setNumber("");
+            setImage("");
+            setAddress("");
+            setError(null);
+            setEmptyField([]);
 
-      console.log("Building added successfully!", json);
-      dispatch({ type: "CREATE_BUILDINGS", payload: json });
-    }
-  };
-  return (
-    <form className="container" onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label className="form-label">Name</label>
-        <input
-          type="text"
-          className={
-            emptyField
-              ? emptyField.includes("name")
-                ? "form-control is-invalid"
-                : "form-control"
-              : "form-control"
-          }
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-      </div>
+            console.log("Building added successfully!", json);
+            dispatch({ type: "CREATE_BUILDINGS", payload: json });
+        }
+    };
+    return (
+        <form className="other-form" onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label className="form-label label">Name</label>
+                <input
+                    type="text"
+                    className={
+                        emptyField
+                            ? emptyField.includes("name")
+                                ? "form-control is-invalid"
+                                : "form-control"
+                            : "form-control"
+                    }
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+            </div>
 
-      <div className="mb-3">
-        <label className="form-label">Number</label>
-        <input
-          type="number"
-          className={
-            emptyField
-              ? emptyField.includes("number")
-                ? "form-control is-invalid"
-                : "form-control"
-              : "form-control"
-          }
-          onChange={(e) => setNumber(e.target.value)}
-          value={number}
-        />
-      </div>
+            <div className="mb-3">
+                <label className="form-label label">Number</label>
+                <input
+                    type="number"
+                    className={
+                        emptyField
+                            ? emptyField.includes("number")
+                                ? "form-control is-invalid"
+                                : "form-control"
+                            : "form-control"
+                    }
+                    onChange={(e) => setNumber(e.target.value)}
+                    value={number}
+                />
+            </div>
 
-      <div className="mb-3">
-        <label className="form-label">Address</label>
-        <input
-          type="text"
-          className={
-            emptyField
-              ? emptyField.includes("address")
-                ? "form-control is-invalid"
-                : "form-control"
-              : "form-control"
-          }
-          onChange={(e) => setAddress(e.target.value)}
-          value={address}
-        />
-      </div>
+            <div className="mb-3">
+                <label className="form-label label">Address</label>
+                <input
+                    type="text"
+                    className={
+                        emptyField
+                            ? emptyField.includes("address")
+                                ? "form-control is-invalid"
+                                : "form-control"
+                            : "form-control"
+                    }
+                    onChange={(e) => setAddress(e.target.value)}
+                    value={address}
+                />
+            </div>
 
-      <div className="mb-3">
-        <label className="form-label">Image</label>
-        <input
-          type="text"
-          className="form-control"
-          onChange={(e) => setImage(e.target.value)}
-          value={image}
-        />
-      </div>
+            <div className="mb-3">
+                <label className="form-label label">Image</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setImage(e.target.value)}
+                    value={image}
+                />
+            </div>
 
-      <div className="mb-3">
-        <input type="submit" className="btn btn-primary" />
-      </div>
+            <div className="mb-3">
+                <button type="submit" className="btn btn-primary">
+                    Create Building
+                </button>
+            </div>
 
-      {error && <div>{error}</div>}
-    </form>
-  );
+            {error && <div>{error}</div>}
+        </form>
+    );
 };
 
 export default CreateBuildingForm;
