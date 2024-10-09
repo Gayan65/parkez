539fd8d8642c8088a35032dd4d7bfe227d134302
Gayan65 <gayan65728@yahoo.com>
2024-10-09 19:27:20 +0300

    card view for the building css changed

--- frontend/src/components/BuildingView.js
+++ frontend/src/components/BuildingView.js
@@ -2,25 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BuildingView = ({ name, number, image, address, link }) => {
-  return (
-    <div className="card mb-3 container" style={{ maxWidth: "540px" }}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          {/* add this img tag later <img src="..." className="img-fluid rounded-start" alt="..." /> */}
+    return (
+        <div
+            className="card mb-3 container custom-card "
+            style={{ maxWidth: "340px" }}
+        >
+            <div className="row g-0">
+                <div className="col-md-4  d-flex align-items-center">
+                    {/* add this img tag later  */}
+                    <img
+                        src="https://via.placeholder.com/110"
+                        className="img-fluid rounded-start"
+                        alt="..."
+                    />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            {" "}
+                            {name} {number}{" "}
+                        </h5>
+                        <p className="card-text">{address}</p>
+                        <Link className="view-link" to={link}>
+                            {" "}
+                            View{" "}
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">
-              {" "}
-              {name} {number}{" "}
-            </h5>
-            <p className="card-text">{address}</p>
-            <Link to={link}> View </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default BuildingView;
