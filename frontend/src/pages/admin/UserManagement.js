bab45c1249bc8caafbe4891f1824958b198df605
Gayan65 <gayan65728@yahoo.com>
2024-11-26 05:29:35 +0200

    add table for the email and admin status

--- frontend/src/pages/admin/UserManagement.js
+++ frontend/src/pages/admin/UserManagement.js
@@ -13,6 +13,28 @@ const UserManagement = () => {
             <p className="paragraph text-justify fw-light fst-italic">
                 {t("note")}
             </p>
+            <form className="other-form">
+                <div className="mb-3">
+                    <label className="form-label label"> Search here.. </label>
+                    <input type="text" className="form-control" />
+                </div>
+            </form>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Admin status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
