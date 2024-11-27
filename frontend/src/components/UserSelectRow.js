6e6a864a7dc51b181be7303ab67e7fe8d211a5ce
Gayan65 <gayan65728@yahoo.com>
2024-11-27 02:21:50 +0200

    remove state

--- frontend/src/components/UserSelectRow.js
+++ frontend/src/components/UserSelectRow.js
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 const UserSelectRow = ({ i, email, status, clickedRow, setClickedRow }) => {
     //translation
     const { t } = useTranslation("usermanagement");
-
-    //status
-    const [clicked, setClicked] = useState(false);
 
     const handleClick = () => {
         setClickedRow(i); // Update the clicked row index in the parent component
