bbf396b5dbd8e8411352e49e250bf9b1cc87efb4
Gayan65 <gayan65728@yahoo.com>
2025-03-09 14:02:09 +0200

    task fetchwrapper add

--- frontend/src/pages/admin/Tasks.js
+++ frontend/src/pages/admin/Tasks.js
@@ -5,10 +5,13 @@ import { useTaskContext } from "../../hooks/useTaskContext";
 //components
 import PendingRequest from "../../components/PendingRequest";
 import PendingUnassignRequest from "../../components/PendingUnassignRequest";
+import Loader from "../../components/Loader";
+import { fetchWrapper } from "../../utils/fetchWrapper";
 
 const Tasks = () => {
     const { user } = useAuthContext();
     const [activeTab, setActiveTab] = useState("assign");
+    const [loader, setLoader] = useState(false);
 
     //task count context
     const { pendingTasks, pendingUnassignTasks, task_dispatch } =
@@ -17,22 +20,36 @@ const Tasks = () => {
     useEffect(() => {
         //fetch number of tasks
         const numberOfTasks = async () => {
-            const response = await fetch("/api/tasks", {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            });
-            const json = await response.json();
-
-            if (response.ok) {
-                task_dispatch({
-                    type: "SET_NUMBER_OF_TOTAL_TASKS",
-                    payload: {
-                        totalTasks: json.totalTasks,
-                        pendingTasks: json.pendingTasks,
-                        pendingUnassignTasks: json.pendingUnassignTasks,
+            try {
+                setLoader(true);
+                const response = await fetchWrapper("/api/tasks", {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`,
                     },
                 });
+
+                if (!response.ok) {
+                    throw new Error(
+                        `Error fetching buildings: ${response.status}`
+                    );
+                }
+
+                const json = await response.json();
+
+                if (response.ok) {
+                    task_dispatch({
+                        type: "SET_NUMBER_OF_TOTAL_TASKS",
+                        payload: {
+                            totalTasks: json.totalTasks,
+                            pendingTasks: json.pendingTasks,
+                            pendingUnassignTasks: json.pendingUnassignTasks,
+                        },
+                    });
+                }
+            } catch (error) {
+                console.error("Error in fetchBuildings:", error);
+            } finally {
+                setLoader(false);
             }
         };
 
